bb99e8ad40ecc2400a401c2d491f075bf14ae2a7
MadhushaPrasad <madushaprasad21@gmail.com>
2022-01-07 09:00:44 +0530

    feat(project):initialize the project

---
+++ src/test/java/com/sha/onlinedeviceshopbackend/OnlineDeviceShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.onlinedeviceshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDeviceShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
